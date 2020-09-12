7625e0efdb275980e05dda173f262670ce9d70bb
”miya” <vividsnoucat@gmail.com>
2020-09-12 13:31:57 +0000

    fc

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,5 +7,22 @@ use App\Http\Controllers\Controller;
 
 class NewsController extends Controller
 {
-    //
+  public function add()
+  {
+      return view('admin.news.create');
+  }
+  public function create()
+  {
+      return redirect('admin/profile/create');
+  }
+
+  public function edit()
+  {
+      return view('admin.profile.edit');
+  }
+
+  public function update()
+  {
+      return redirect('admin/profile/edit');
+  }
 }
